dfc006f1c08b0fcc696088a8cfbfba7168366fdf
hazg <hazg@mail.ru>
2015-07-22 14:43:35 +0300

    3-0-stable, russian locale

--- app/overrides/add_create_all_button.rb
+++ app/overrides/add_create_all_button.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-  :virtual_path => "spree/admin/variants/index",
-  :name         => "add_create_all_button",
-  :insert_top   => "[data-hook='toolbar'], #toolbar[data-hook]",
-  :disabled     => false,
-  :partial      => "spree/admin/variants/create_all")
+  :virtual_path   => "spree/admin/variants/index",
+  :name           => "add_create_all_button",
+  :insert_bottom  => "#new_variant",
+  :sequence       => 'auth_shared_login_bar',
+  :disabled       => false,
+  :partial        => "spree/admin/variants/create_all")
