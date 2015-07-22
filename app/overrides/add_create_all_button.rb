390d2b83e1a0a76cad72d1a6d385213f16c83691
hazg <hazg@mail.ru>
2015-07-22 15:26:25 +0300

    up

--- app/overrides/add_create_all_button.rb
+++ app/overrides/add_create_all_button.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   :virtual_path   => "spree/admin/variants/index",
   :name           => "add_create_all_button",
-  :insert_bottom  => "#new_variant",
-  :sequence       => 'auth_shared_login_bar',
+  :replace        => 'erb[loud]:contains("button_link_to(Spree.t(:new_variant)")',
   :disabled       => false,
   :partial        => "spree/admin/variants/create_all")
