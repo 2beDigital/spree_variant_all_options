ec043da963ecf8da8693b5f6a0e3e5202cf4eaee
SergiCarrasco <sergi@2bedigital.com>
2016-05-04 02:40:02 +0200

    v1.0.0: Sku generated automatically on CreateAll

--- app/controllers/spree/admin/variants_controller_decorator.rb
+++ app/controllers/spree/admin/variants_controller_decorator.rb
@@ -12,11 +12,18 @@ Spree::Admin::VariantsController.class_eval do
 
     options.each do |ids|
       v = @product.variants.new
+      sku = @product.sku
       if ids.kind_of?(Array)
         v.option_value_ids = ids.flatten
       else
         v.option_value_ids = ids
       end
+      v.option_value_ids.each do |ovid|
+        ov=Spree::OptionValue.find(ovid)
+        sku="#{sku} #{ov.presentation[0...3].upcase}"
+      end
+      pr=Spree::Variant.find_by(sku: sku)
+      if (!pr) then v.sku=sku end
       v.price = @product.price
       v.weight = @product.weight
       v.height = @product.height
